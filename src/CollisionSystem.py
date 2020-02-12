9a062e58a0954caed8639d0c2e5a78efea40c379
Fanurs <fyanurs@gmail.com>
2020-02-12 08:51:48 -0500

    In-class exercise: Added a more descriptive docstring

--- src/CollisionSystem.py
+++ src/CollisionSystem.py
@@ -1,5 +1,20 @@
 class CollisionSystem:
     def __init__(self, proj=None, targ=None, skyrme=None, energy=None, imp_param=None):
+        """
+        Initialize the collision system.
+
+        `proj`: A `str` to describe the projectile nucleus.
+
+        `targ`: A `str` to describe the target nucleus.
+
+        `skyrme`: The Skyrme number. Actual Skyrme parameters have to be known.
+
+        `energy`: Beam energy in $MeV/u$.
+        
+        `imp_param`: Impact parameter in fm.
+
+        """
+
         self.proj_symb = (''.join(re.findall('[A-z]+', proj))).lower()
         self.proj_A = int(''.join(re.findall('[0-9]+', proj)))
         self.targ_symb = (''.join(re.findall('[A-z]+', targ))).lower()

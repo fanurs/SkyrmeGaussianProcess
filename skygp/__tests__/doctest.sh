645547c013732308b2435ea330b9a266949a2a47
Fanurs <fyanurs@gmail.com>
2020-03-13 20:39:15 -0400

    Exclude __init__.py from doctest.

--- skygp/__tests__/doctest.sh
+++ skygp/__tests__/doctest.sh
@@ -1,1 +1,18 @@
-python -m doctest -o ELLIPSIS ../*.py
+# This script runs all the .py files in skygp with doctest except __init__.py
+# If the following command doesn't work,
+#		$ ./doctest.sh
+# try
+#		$ bash doctest.sh
+
+for python_file in ../*.py
+do
+	basename=$(basename -- $python_file)
+	if [[ $basename == '__init__.py' ]]
+	then
+		echo 'skipping ' $python_file
+		continue
+	else
+		echo 'doctesting ' $python_file
+		python -m doctest -o ELLIPSIS $python_file
+	fi
+done
